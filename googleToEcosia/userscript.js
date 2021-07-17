fa91998f63febe3f114c253d9b80f637f1b348bd
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-17 17:13:35 -0400

    Code cleanup and replace Google logo

--- googleToEcosia/userscript.js
+++ googleToEcosia/userscript.js
@@ -1,19 +1,22 @@
 // ==UserScript==
-// @name        New script - google.com
+// @name        Search Ecosia but on google
 // @namespace   Violentmonkey Scripts
 // @match       https://www.google.com/
 // @grant       none
-// @version     1.0
-// @author      -
+// @version     2.0
+// @author      TheTechRobo
 // @description 19/06/2021, 14:07:49
 // ==/UserScript==
 var arr = document.querySelectorAll('.gNO89b');
 for (var i=0;i<arr.length;i++) {
-    //arr[i].onclick = "void();return false"
-  arr[i].value = "fdgh.";
+  arr[i].value = "Search Ecosia";
 }
 var arr = document.querySelectorAll('form');
 for (var i=0;i<arr.length;i++) {
-    //arr[i].onclick = "void();return false"
-  arr[i].action = "https://thetechrobo.github.io";
+  arr[i].action = "https://www.ecosia.org/search";
+}
+var arr = document.querySelectorAll('.lnXdpd');
+for (var i=0;i<arr.length;i++) {
+  arr[i].src = "https://www.doz.com/cms/wp-content/uploads/2014/08/ecosia-logo.jpg"
+  arr[i].srcset = "https://www.doz.com/cms/wp-content/uploads/2014/08/ecosia-logo.jpg"
 }
