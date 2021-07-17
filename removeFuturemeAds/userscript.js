6b90163397002c463ab5dfcba854782faa554b1c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-17 17:01:43 -0400

    Fading out looks cooler :sunglasses:

--- removeFuturemeAds/userscript.js
+++ removeFuturemeAds/userscript.js
@@ -3,10 +3,10 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.futureme.org/*
 // @grant       none
-// @version     1.0.1
+// @version     1.0.2
 // @author      TheTechRobo
 // @description 16/06/2021, 11:37:46
 // @run-at      document-body
 // ==/UserScript==
-$('.ad').hide();
+$('.ad').fadeOut();
 console.log("FutureMe ads should be removed!");
