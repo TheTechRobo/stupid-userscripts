8518e29742851c5a54d0f45a1227824681408c36
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-17 17:20:40 -0400

    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    vDO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE
    DO NOT USE

---
+++ doNotClickMe/imSerious/doYouWantToTortureYourself/thenHereYouGo/userscript.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        torture yourself
+// @namespace   Violentmonkey Scripts
+// @match       *://*/*
+// @grant       none
+// @version     4.1
+// @author      TheTechRobo
+// @description 19/06/2021, 15:56:25
+// @name        torture yourself
+// ==/UserScript==
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+console.log("WARNING: TORTURE DEVICE IN USE. RECOMMENDED TO DISABLE.")
+async function thingy() {
+  if (document.body.innerHTML.indexOf("baguette") >= 0) {
+    alert("nope");
+}
+               }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function hi() {
+  for (;;) { await thingy();
+          console.log(1);
+  await sleep(30000);
+         }
+}
+
+(async() => {
+  await hi()
+})()
