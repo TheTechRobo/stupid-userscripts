168a1584803e09b38bc98b211845510146322e6c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-17 17:15:39 -0400

    Create userscript2-optional.js

---
+++ googleToEcosia/userscript2-optional.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        New script - google.com searcheufdiughwfeiuwhgriefdsvibhguvh
+// @namespace   Violentmonkey Scripts
+// @match       https://www.google.com/search
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 19/06/2021, 14:07:49
+// ==/UserScript==
+document.body.innerHTML = "HOW DID YOU GET PAST MY AMAZiNG SECURITY "
