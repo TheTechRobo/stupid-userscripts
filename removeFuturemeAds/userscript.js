7a7dcab1709a83f76745dff785bf36dc5285037d
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-17 17:04:11 -0400

    Add downloadURL and updateURL
    Who knows if it'll work

--- removeFuturemeAds/userscript.js
+++ removeFuturemeAds/userscript.js
@@ -3,10 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.futureme.org/*
 // @grant       none
-// @version     1.0.2
+// @version     1.0.3
 // @author      TheTechRobo
 // @description 16/06/2021, 11:37:46
 // @run-at      document-body
+// @downloadURL https://raw.githubusercontent.com/TheTechRobo/stupid-userscripts/main/removeFuturemeAds/userscript.js
+// @updateURL   https://raw.githubusercontent.com/TheTechRobo/stupid-userscripts/main/removeFuturemeAds/userscript.js
 // ==/UserScript==
 $('.ad').fadeOut();
 console.log("FutureMe ads should be removed!");
